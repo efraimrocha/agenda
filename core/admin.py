1676a941a25d9c0e4025a3d4762124d73ec3188a
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-02-04 07:08:56 -0300

    Add filter function in EventoAdmin class

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,7 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('title', 'event_date', 'creation_date', 'decripption')
-
+    list_filter = ('user', 'event_date','creation_date',)
 
 
 admin.site.register(Evento, EventoAdmin)
