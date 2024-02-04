bbb976f6bf9f2611f1407df6446a0ba1a82bd651
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2024-02-04 06:26:14 -0300

    Creates listing screen, and separate html structure

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
+
+# def index(request):
+#     return redirect('/agenda/')
+    
 
 def lista_eventos(request):
     evento = Evento.objects.all()
